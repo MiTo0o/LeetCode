b036e8ee07f556502bf11b8b0036db8ca488fe01
Derzan Chiang <76019114+MiTo0o@users.noreply.github.com>
2023-07-15 21:51:55 -0400

    Time: 45 ms (83.86%), Space: 16.2 MB (88.88%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,14 +1,10 @@
-from collections import defaultdict
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        old_count = defaultdict(int)
-        for letter in s:
-          old_count[letter] += 1
-        
-        new_count = defaultdict(int)
-        for letter in t:
-          new_count[letter] += 1
-        
-        for letter in new_count:
-          if new_count[letter] != old_count[letter]:
-            return letter
+        ascii = 0
+        for i in t:
+          ascii += ord(i)
+          
+        for i in s:
+          ascii -= ord(i)
+          
+        return chr(ascii)
